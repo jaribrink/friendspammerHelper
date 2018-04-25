38562dcaca59d37c89643f8a0fa3d5732d34f514
jaribrink <jari.vandenbrink@student.hu.nl>
2018-04-25 12:09:08 +0200

    added testing class

---
+++ src/test/java/nl/hu/sie/bep/friendspammer/Testing.java
@@ -0,0 +1,12 @@
+package nl.hu.sie.bep.friendspammer;
+
+import static org.junit.Assert.fail;
+import org.junit.Test;
+
+public class Testing {
+
+    @Test
+    public void test_fail() {
+        fail("Let's test the failing unit test");
+    }
+}
