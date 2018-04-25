24d8be86ff84c3d59c64cd8448f66f75cac64d66
jaribrink <jari.vandenbrink@student.hu.nl>
2018-04-25 11:55:11 +0200

    fixed issues

--- src/main/java/nl/hu/sie/bep/friendspammer/MongoSaver.java
+++ src/main/java/nl/hu/sie/bep/friendspammer/MongoSaver.java
@@ -1,51 +1,57 @@
 package nl.hu.sie.bep.friendspammer;
 
-import com.mongodb.*;
-import com.mongodb.client.MongoCollection;
-import com.mongodb.client.MongoDatabase;
+import java.net.UnknownHostException;
+import java.util.Arrays;
+
 import org.bson.Document;
 
-import java.net.UnknownHostException;
-
-import static sun.plugin.javascript.navig.JSType.Document;
+import com.mongodb.BasicDBObject;
+import com.mongodb.DB;
+import com.mongodb.DBCollection;
+import com.mongodb.MongoClient;
+import com.mongodb.MongoClientOptions;
+import com.mongodb.MongoClientURI;
+import com.mongodb.MongoCredential;
+import com.mongodb.MongoException;
+import com.mongodb.ServerAddress;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
 
 public class MongoSaver {
-	
+
 	public static boolean saveEmail(String to, String from, String subject, String text, Boolean html) {
 		String userName = "JariBrink";
 		String password = "wachtwoord";
 		String database = "friendspammer";
-		
+
 		MongoCredential credential = MongoCredential.createCredential(userName, database, password.toCharArray());
-		
+
 		boolean success = true;
-		
+
 		try (MongoClient mongoClient = new MongoClient(new ServerAddress("YOUR HOST", 27939), credential, MongoClientOptions.builder().build()) ) {
-			
+
 			MongoDatabase db = mongoClient.getDatabase( database );
-			
+
 			MongoCollection<Document> c = db.getCollection("email");
-			
+
 			Document  doc = new Document ("to", to)
-			        .append("from", from)
-			        .append("subject", subject)
-			        .append("text", text)
-			        .append("asHtml", html);
+					.append("from", from)
+					.append("subject", subject)
+					.append("text", text)
+					.append("asHtml", html);
 			c.insertOne(doc);
 		} catch (MongoException mongoException) {
 			System.out.println("XXXXXXXXXXXXXXXXXX ERROR WHILE SAVING TO MONGO XXXXXXXXXXXXXXXXXXXXXXXXXX");
 			mongoException.printStackTrace();
 			success = false;
 		}
-		
+
 		return success;
- 		
-	}
-	
-	
-	public static void main(String ...args) throws UnknownHostException {
-		
-		System.out.println("test");
+
 	}
 
+	public static void main(String ...args) throws UnknownHostException {
+
+		System.out.println("test");
+	}
 }
