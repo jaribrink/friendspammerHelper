34034970bea068eef16d67803454e5015b8773d4
jaribrink <jari.vandenbrink@student.hu.nl>
2018-05-07 22:27:49 +0200

    added travis file for sonar

--- src/test/java/nl/hu/sie/bep/friendspammer/Testing.java
+++ src/test/java/nl/hu/sie/bep/friendspammer/Testing.java
@@ -1,12 +1,15 @@
 package nl.hu.sie.bep.friendspammer;
 
 import static org.junit.Assert.fail;
+
+import org.junit.Ignore;
 import org.junit.Test;
 
+@Ignore
 public class Testing {
 
-//    @Test
-//    public void test_fail() {
-//        fail("Let's test the failing unit test");
-//    }
+    @Test
+    public void test_fail() {
+        fail("Let's test the failing unit test");
+    }
 }
